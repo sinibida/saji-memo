217cf6f7e11d059025f8a468263a4df7a9576280
SPAUPA <40449398+sinibida@users.noreply.github.com>
2026-08-12 14:36:52 +0000

    note order to created At

--- src/services/apiRefMemos.ts
+++ src/services/apiRefMemos.ts
@@ -1,6 +1,6 @@
-import { collection } from "firebase/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 export default function apiRefMemos() {
-    return collection(db, "memos");
+    return query(collection(db, "memos"), orderBy("createdAt", "desc"));
 }
